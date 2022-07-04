55259e508fc4e682c2bf6535d364cd0ccacd6895
santerijps <santeri.sydanmetsa@pm.me>
2022-07-04 10:03:37 +0300

    refactor: update basic example

--- examples/basic.c
+++ examples/basic.c
@@ -4,30 +4,27 @@
 int main (int argc, char **argv) {
 
   FLAG_PARSER parser;
-  FLAG *name, *age, *is_single, *quote, *book;
+  FLAG *name, *age, *is_single, *quote, *book, *citizenship;
   CLARGS other_args;
+  int i;
 
   parser = clargs_parser_new(argc, argv);
   name = clargs_flag_new_str(&parser, "name", 'n', "Name of the person.", "Anonymous");
   age = clargs_flag_new_int(&parser, "age", 'a', "Age of the person.", -123);
   is_single = clargs_flag_new_bool(&parser, "single", 's', "Is the person single?", 0);
-  quote = clargs_flag_new_str(&parser, "quote", 'q', "Favourite quote.", "No quote");
-  book = clargs_flag_new_str(&parser, "book", 'b', "The favorite book.", "Harry Potter");
 
   other_args = clargs_parse_args(&parser);
 
   printf("name: %s\n", name->value._str);
   printf("age: %d\n", age->value._int);
   printf("is single: %s\n", is_single->value._bool ? "true" : "false");
-  printf("quote: %s\n", quote->value._str);
-  printf("book: %s\n", book->value._str);
-  printf("other args %d\n", other_args.size);
+  printf("other args count: %d\n", other_args.size);
 
-  for (int i = 0; i < other_args.size; i++) {
+  for (i = 0; i < other_args.size; i++) {
     printf("Other arg: %s\n", other_args.values[i]);
   }
 
-  clargs_print_usage(&parser);
+  clargs_print_flag_usage(&parser, 0);
 
   return 0;
 }
